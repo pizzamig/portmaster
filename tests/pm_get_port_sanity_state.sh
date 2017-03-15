fa17668e0151b958849caec159b192572dc042ed
Luca Pizzamiglio <luca.pizzamiglio@gmail.com>
2017-03-15 12:05:30 +0100

    Adding pm_get_port_sanity_state and its tests

---
+++ tests/pm_get_port_sanity_state.sh
@@ -0,0 +1,34 @@
+#!/bin/sh
+. ../portmaster.subr
+
+# test cases
+test_pm_get_port_sanity_state()
+{ # BROKEN case
+	local state _cwd
+	_cwd=$(pwd)
+	state=$(pm_get_port_sanity_state devel/broken)
+	assertEquals "BROKEN" "$state"
+	assertEquals "$_cwd" "$(pwd)"
+}
+
+test_pm_get_port_sanity_state_2()
+{ # FORBIDDEN port
+	local state
+	state=$(pm_get_port_sanity_state devel/forbidden)
+	assertEquals "FORBIDDEN" "$state"
+}
+
+test_pm_get_port_sanity_state_3()
+{ # IGNORE port
+	local state
+	state=$(pm_get_port_sanity_state devel/ignore)
+	assertEquals "IGNORE" "$state"
+}
+
+setUp()
+{
+	pd=./faketree
+}
+
+# loading shunit2
+. ../shunit2/source/2.1/src/shunit2
